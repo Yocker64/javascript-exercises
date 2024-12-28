822bd81070df78988799dd7af6bf07da4c309a97
Sandoval Mario <mdyoc74@gmail.com>
2024-12-28 12:18:29 +0900

    Solve 5th exercise sumAll: passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,38 @@
-const sumAll = function() {
+const sumAll = function(firstInput, secondInput) {
 
+    if( firstInput < 0 || secondInput < 0){
+        return 'ERROR'
+    }
+    if( typeof firstInput != "number" || typeof secondInput != "number"){
+        return 'ERROR'
+    }
+    
+    
+    if (!(Number.isInteger(firstInput) && Number.isInteger(secondInput))) {
+        return 'ERROR'
+    }
+
+    if (firstInput > secondInput) {
+        num2 = firstInput;
+        num1 = secondInput;
+    }else{
+        num1 = firstInput;
+        num2 = secondInput;
+    }
+
+
+    const TIMES_TO_REPEAT = num2 - num1;
+    const FIRST_NUMBER = num1;
+    let sum = 0;
+    for (let index = 0; index <= TIMES_TO_REPEAT; index++) {
+        sum += FIRST_NUMBER + index;
+    }
+    console.log(typeof num1);
+  
+    console.log(typeof sum);
+    console.log(sum);
+       
+    return sum;
 };
 
 // Do not edit below this line
