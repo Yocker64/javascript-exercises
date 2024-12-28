48093698c4cd331123872c917456c5c9928a4209
Sandoval Mario <mdyoc74@gmail.com>
2024-12-28 12:36:25 +0900

    Solve problem 7: tempConversion (all tests passed)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFah) {
+  console.log(typeof ((tempFah-32)*5/9).toFixed(1));
+  
+  return parseFloat(((tempFah-32)*5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  return parseFloat((tempCel*9/5 +32).toFixed(1));
 };
 
 // Do not edit below this line
