14f66f39a8c96402563d03afb9228d9506ef2afc
Sandoval Mario <mdyoc74@gmail.com>
2024-12-28 11:43:56 +0900

    Solve 4th problem, removeFromArray. All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,33 @@
 const removeFromArray = function() {
+    
+    const args = [...arguments];
+    let array = args[0];
+    let resultArray = [];
+    let indexResult = 0;
+    let copy = false;
+    for (let indexArray = 0; indexArray < array.length; indexArray++) {
+        for (let indexDel = 1; indexDel < arguments.length; indexDel++) {
+            if (array[indexArray] === arguments[indexDel]) {
+                copy = false;
+                break;
+            }
+                copy = true;
+        }
+        if (copy == true) {
+            resultArray[indexResult] = array[indexArray];
+            indexResult ++;
+        }
+        
+    }
+
+    console.log(resultArray);
+    console.log(arguments);
+    console.log(array);
+    
+    
+    
+    
+    return resultArray;
 };
 
 // Do not edit below this line
