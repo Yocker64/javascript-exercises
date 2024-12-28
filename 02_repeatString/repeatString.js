b6eb845a00edd95a0c37fd451594062a741340ce
Sandoval Mario <mdyoc74@gmail.com>
2024-12-28 10:28:48 +0900

    Add features to: 01_helloWorld (say hello to world), and to 02_repeatString (repeatstring and handle negative integers and blank strings as arguments)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let finalString = '';
 
-};
+    if (times<0) {
+        return "ERROR";
+    }else{
+        for (let index = 0; index < times; index++) {
+            finalString += str;
+        }
+        return finalString;
+    };
+    }
+    
 
 // Do not edit below this line
 module.exports = repeatString;
