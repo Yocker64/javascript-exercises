4bc014fe67b8ab2c8c4346908309f7443eb06680
Sandoval Mario <mdyoc74@gmail.com>
2025-01-19 20:55:21 +0900

    Add solution to the 8th javascript exercise: calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length === 0) {
+    return 0
+  }else{
+    return arr.reduce((total, acc)=>{
+      total = total + acc;
+      return total;  
+    })
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, act)=> {
+    total = total* act;
+    return total;
+  })
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  }else{
+    return number * factorial(number-1);
+  }
 };
 
 // Do not edit below this line
