2764d160ac15e451e11c3bdfe0d710b4d3882edf
Sandoval Mario <mdyoc74@gmail.com>
2025-01-20 09:59:28 +0900

    Add solution to the 9th exercise: palindromes.js, can handle signs, blank spaces and numbers.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let splittedStr =str.toLowerCase().split('');
+    console.log(splittedStr);
+    
+    let onlyLetters =  splittedStr.filter((item)=>{
+        return (item.charCodeAt(0) > 96 && item.charCodeAt(0) < 123) || (item.charCodeAt(0) > 47 && item.charCodeAt(0) < 58);
+    });
+    let arrLen = onlyLetters.length;
+    console.log(arrLen);
+    
+    for (let i = 0; i < arrLen; i++) {
+        if ((onlyLetters[i] == onlyLetters[arrLen-i-1])) {
+            continue;
+        }else{
+            return false;
+        }
+        
+    }   
+    console.log(onlyLetters);
+    
+    return true; 
 };
 
 // Do not edit below this line
