b766c7c025c8202cc26d203c88b3772d58c3bc09
Sandoval Mario <mdyoc74@gmail.com>
2025-01-20 15:40:00 +0900

    Add solution to the 12th exercise, findTheOldest: find the oldest among several objects based on their age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
+const findTheOldest = function(people) {
+    return people.reduce((a, b) =>{
+    const currentAge = getAge(a.yearOfBirth, a.yearOfDeath);
+    const oldestAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        return oldestAge < currentAge? a : b;
+    })
 };
 
 // Do not edit below this line
