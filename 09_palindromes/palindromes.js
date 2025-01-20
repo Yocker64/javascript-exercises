941587693350a6e359d67b298ec6472d1605cade
Sandoval Mario <mdyoc74@gmail.com>
2025-01-20 11:01:48 +0900

    Add solution to the 11th exercise, getTheTitles: return an array with the name of books from an array of objects

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,24 +1,12 @@
 const palindromes = function (str) {
-    let splittedStr =str.toLowerCase().split('');
-    console.log(splittedStr);
-    
-    let onlyLetters =  splittedStr.filter((item)=>{
+
+    //First lowers all the uppercase letters, then it splits the string into an array and filters that array if it is a number or a letter from the English alphabet
+    let onlyLetters = str.toLowerCase().split('').filter((item)=>{
         return (item.charCodeAt(0) > 96 && item.charCodeAt(0) < 123) || (item.charCodeAt(0) > 47 && item.charCodeAt(0) < 58);
-    });
-    let arrLen = onlyLetters.length;
-    console.log(arrLen);
-    
-    for (let i = 0; i < arrLen; i++) {
-        if ((onlyLetters[i] == onlyLetters[arrLen-i-1])) {
-            continue;
-        }else{
-            return false;
-        }
-        
-    }   
-    console.log(onlyLetters);
-    
-    return true; 
+    }).join('');
+    let reverse = onlyLetters.split('').reverse().join('');
+    return onlyLetters === reverse;
+
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //returns  an array with the titles of the books in order
+    return books.map((book) => book.title);
 };
 
 // Do not edit below this line
